3878d6ea28f460618d436f854e430d16ed48e27d
JimmyCJM <chijieming@sina.com>
2017-06-24 23:03:12 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -5,7 +5,7 @@ function student(name,age,klass){
   this.name = name;
   this.age = age;
   this.introduce = function (){
-    var a = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.class+".";  
+    var a = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.klass+".";  
     return a;
   }
 };
