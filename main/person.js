ef701424b6ca5366f1ed560ba496345b63dd1b90
JimmyCJM <chijieming@sina.com>
2017-06-24 23:15:22 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -2,7 +2,7 @@ var Person = function(name,age){
   this.name = name;
   this.age = age;
   this.introduce = function (){
-    return "My name is " + this.name + ". I am " + "this.age" + " years old.";
+    return "My name is " + this.name + ". I am " + this.age + " years old.";
 
   }
 }
