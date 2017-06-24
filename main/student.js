c3eaf0d753d994fce7f3e91825bd27d54ac5bdfc
JimmyCJM <chijieming@sina.com>
2017-06-24 22:59:12 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,13 @@
 let Person = require('./person')
-// Write your code here
+function student(name,age,klass){
+  Person.call(this);
+  this.klass = klass;
+  this.name = name;
+  this.age = age;
+  this.introduce = function (){
+    var a = "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.class+".";  
+    return a;
+  }
+};
+Chinese.prototype = Person.prototype;}
 module.exports = Student
